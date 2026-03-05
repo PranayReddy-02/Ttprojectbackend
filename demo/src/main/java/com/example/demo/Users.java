9dade35bacfd881cb974fcc8546a16c100a9da00
pallepranayreddy128 <pallepranayreddy128@gmail.com>
2026-03-05 14:16:36 +0530

    ihfyuvg

--- demo/src/main/java/com/example/demo/Users.java
+++ demo/src/main/java/com/example/demo/Users.java
@@ -4,15 +4,48 @@ import jakarta.persistence.*;
 @Entity
 public class Users {
 
+    private static final String Long = null;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String username;
     private String email;
+    private String username;
+    private String password;
+
+
+    public Users(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getEmail() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEmail'");
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
 }
